d5389d65a0098a4ec4a9c54f180de41dad42278b
Alex Spencer <alexander.j.spencer@gmail.com>
2019-07-12 15:09:08 -0700

    Updated the JS code to use the isEven function rather than writing function in code file

--- app.js
+++ app.js
@@ -6,12 +6,26 @@ const buttonPress = document.getElementById('on-click');
 const imageEven = document.getElementById('even-image');
 const imageOdd = document.getElementById('odd-image');
 
-buttonPress.onclick = function execute() {
-    const numberValue = Number(numberEnter.value); {
-        if(numberValue % 2 === 0) {
-            return imageEven.classList.remove('hidden');
-        } else {
-            return imageOdd.classList.remove('hidden');
-        }
+buttonPress.onclick = function execute () {
+    const numberFigure = Number(numberEnter.value);
+    const resultingNumber = isEven(numberFigure);
+    if(resultingNumber === true) {
+        return imageEven.classList.remove('hidden');
+    } else {
+        return imageOdd.classList.remove('hidden');
     }
 };
+
+
+
+
+
+//buttonPress.onclick = function execute() {
+    //const numberValue = Number(numberEnter.value); {
+        //if(numberValue % 2 === 0) {
+         //   return imageEven.classList.remove('hidden');
+        //} else {
+         //   return imageOdd.classList.remove('hidden');
+       // }
+   // }
+//};
